4b75a8652388e319130741cb48cc1626a153161c
rhmn68 <auliar68@gmail.com>
2019-12-09 15:41:29 +0700

    hari pertama

---
+++ app/src/main/java/com/rahmanaulia/latihanview/CourtCounterActivity.kt
@@ -0,0 +1,12 @@
+package com.rahmanaulia.latihanview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CourtCounterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_court_counter)
+    }
+}
